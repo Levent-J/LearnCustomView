818e065170acdaedadab7c5952724c420c864339
Levent_J <liwenjing960110@gmail.com>
2016-09-16 20:53:35 +0800

    add wrap_content

--- app/src/main/java/com/levent_j/learncustomview/widget/CircleView.java
+++ app/src/main/java/com/levent_j/learncustomview/widget/CircleView.java
@@ -47,4 +47,20 @@ public class CircleView extends View{
         int radius = Math.min(width,height)/2;
         canvas.drawCircle(width/2,height/2,radius,mPaint);
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthSpecMode==MeasureSpec.AT_MOST&&heightSpecMode==MeasureSpec.AT_MOST){
+            setMeasuredDimension(200,200);
+        }else if (widthSpecMode==MeasureSpec.AT_MOST){
+            setMeasuredDimension(200,heightSpecSize);
+        }else if (heightSpecMode==MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthSpecSize,200);
+        }
+    }
 }
